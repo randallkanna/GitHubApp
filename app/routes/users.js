0ca2ab94e57b8d7c993d425758ea74a1a368d56c
Randall <randallkanna@gmail.com>
2015-03-02 15:42:41 -0800

    adding search bar on navbar

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,10 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function() {
 		debugger
+		
 		var github = new Github({
 		  token: "8b0c0d2d6e5cb3017ddd87e7f243152afcc66698",
 		  auth: "oauth"
 		});
+
 		var user = github.getUser();
 		var repo = github.getRepo("randalldiann", "FIRSTREPO");
 		debugger
