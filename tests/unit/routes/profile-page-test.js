348e3b5e46b034e2776ad25fc6290cd4d926287d
Randall <randallkanna@gmail.com>
2015-03-03 11:50:18 -0800

    adding skeleton for empty tests. Fix on controller index to add ember

---
+++ tests/unit/routes/profile-page-test.js
@@ -0,0 +1,57 @@
+// Tests for individual user profile page
+
+// Unit Tests
+
+import {
+  moduleFor,
+  test
+} from 'ember-qunit';
+
+moduleFor('', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  // var route = this.subject();
+  // assert.ok(route);
+
+  // assert.ok()
+});
+
+test('A user has a username', function(assert) {
+	// var username = 
+
+});
+
+test('A user has a profile image', function(assert) {
+
+});
+
+test('A user has a name', function(assert) {
+
+});
+
+test('A user has a company', function(assert) {
+
+});
+
+test('A user has a blog URL', function(assert) {
+
+});
+
+test('A user has a follower count', function(assert) {
+
+});
+
+test('A user has a following count', function(assert) {
+
+});
+
+test('A user has an account creation date', function(assert) {
+
+	// assert.ok()
+});
+
+
+
